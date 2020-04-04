d07c7e224001bbd48b8fd2032eb01728f2b75453
Захаров Андрей <zakharov_a@tochka.com>
2020-04-04 19:15:15 +0500

    init commit

---
+++ src/main/java/com/github/morvell/lsn/LsnApplication.java
@@ -0,0 +1,14 @@
+package com.github.morvell.lsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsnApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LsnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/morvell/lsn/LsnApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.morvell.lsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class LsnApplicationTests {
+
+    @Test void contextLoads() {
+
+    }
+
+}
