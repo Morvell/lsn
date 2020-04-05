1b0f6a570aa4f6165a131625025d026dc5559b1c
Захаров Андрей <zakharov_a@tochka.com>
2020-04-05 13:34:59 +0500

    Дописал необходимые методы на контроллер

--- src/main/java/com/github/morvell/lsn/controller/MessageController.java
+++ src/main/java/com/github/morvell/lsn/controller/MessageController.java
@@ -22,31 +22,54 @@ public class MessageController {
 
     private int counter = 4;
 
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
-    }};
+    private List<Map<String, String>> messages = new ArrayList<>() {
+
+        {
+            add(new HashMap<>() {
+
+                {
+                    put("id", "1");
+                    put("text", "First message");
+                }
+            });
+            add(new HashMap<>() {
+
+                {
+                    put("id", "2");
+                    put("text", "Second message");
+                }
+            });
+            add(new HashMap<>() {
+
+                {
+                    put("id", "3");
+                    put("text", "Third message");
+                }
+            });
+        }
+    };
 
     @GetMapping
     public List<Map<String, String>> list() {
+
         return messages;
     }
 
     @GetMapping("{id}")
     public Map<String, String> getOne(@PathVariable String id) {
+
         return getMessage(id);
     }
 
     private Map<String, String> getMessage(@PathVariable String id) {
-        return messages.stream()
-                .filter(message -> message.get("id").equals(id))
-                .findFirst()
+
+        return messages.stream().filter(message -> message.get("id").equals(id)).findFirst()
                 .orElseThrow(NotFoundException::new);
     }
 
     @PostMapping
     public Map<String, String> create(@RequestBody Map<String, String> message) {
+
         message.put("id", String.valueOf(counter++));
 
         messages.add(message);
@@ -55,7 +78,9 @@ public class MessageController {
     }
 
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+    public Map<String, String> update(@PathVariable String id,
+            @RequestBody Map<String, String> message) {
+
         Map<String, String> messageFromDb = getMessage(id);
 
         messageFromDb.putAll(message);
@@ -66,6 +91,7 @@ public class MessageController {
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable String id) {
+
         Map<String, String> message = getMessage(id);
 
         messages.remove(message);
